39770ce2f6ba46f370c74a68a715f49231bc4612
jeexare <hgrubiof@gmail.com>
2025-04-18 12:35:59 -0600

    added some projects

--- content.js
+++ content.js
@@ -7,36 +7,36 @@ const portfolioContent = {
         { title: "Contact Me", link: "#poscontactme" }
       ],
       header: {
-        title: "Web development",
-        subtitle: "And then this"
+        title: "front-end dev",
+        subtitle: "and whatever's next"
       },
       aboutMe: {
         paragraph:
-          "Hi there! I'm Gabriela Rubio — a CS student at the University of Utah who’s passionate about making programming both aesthetic and functional. I'm a proud hobby-hopper who loves trying all sorts of things, and I think my code should reflect that creative chaos. So here’s my portfolio — check it out if you think we can twin! Bye world!"
+          "Hi there! I'm Gabriela Rubio — a Computer Science student at the University of Utah who’s passionate about making programming both aesthetic and functional. I'm a proud hobby-hopper who loves trying all sorts of things, and I think my code should reflect that creative chaos. So here’s my portfolio — check it out if you think we can work together!"
       },
       contactMe: {
         title: "let's make something fun",
-        subtitle: "say hi"
+        subtitle: "say hi ~"
       }
     },
     spa: {
       navLinks: [
         { title: "Inicio", link: "#posheader" },
-        { title: "Sobre Mi", link: "#posaboutme" },
+        { title: "Sobre Mí", link: "#posaboutme" },
         { title: "Proyectos", link: "#posprojects" },
-        { title: "Contactame", link: "#poscontactme" }
+        { title: "Contáctame", link: "#poscontactme" }
       ],
       header: {
-        title: "Desarrollo web",
-        subtitle: "Y luego esto"
+        title: "Desarrolladora front-end",
+        subtitle: "y lo que venga después"
       },
       aboutMe: {
         paragraph:
-          "¡Hola! Soy Gabriela Rubio, estudiante de CS en la Universidad de Utah, y me apasiona hacer que la programación sea tanto estética como funcional. Me encanta probar de todo como hobby, y creo que mi código debe reflejar ese caos creativo. Así que aquí está mi portafolio, ¡échale un vistazo si crees que podemos match! Bye world!"
+          "¡Hola! Soy Gabriela Rubio, estudiante de Ciencias de la Computación en la Universidad de Utah, y me apasiona hacer que la programación sea tanto estética como funcional. Me encanta probar cosas nuevas, y creo que mi código debe reflejar ese caos creativo. Así que aquí está mi portafolio, ¡échale un vistazo si crees que podemos trabajar juntos!"
       },
       contactMe: {
-        title: "Creemos algo divertido!",
-        subtitle: "di hola"
+        title: "¡Creemos algo divertido!",
+        subtitle: "di hola ~"
       }
     }
   };
